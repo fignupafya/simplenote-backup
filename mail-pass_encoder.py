6854ac049b2c1810760187058975ea1b76f72024
fignupafya <99953314+fignupafya@users.noreply.github.com>
2023-04-16 00:17:58 +0300

    Add files via upload

---
+++ mail-pass_encoder.py
@@ -0,0 +1,11 @@
+import base64
+
+
+mail = base64.b64encode(input("Mail: ").encode('utf-8')).decode()
+password = base64.b64encode(input("Password: ").encode('utf-8')).decode()
+
+print("\n\nEncoded mail:")
+print(mail)
+
+print("\nEncoded password:")
+print(password)
