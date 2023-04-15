93e07988f45c089d456bb0c9d8391c6fc7c2907a
fignupafya <99953314+fignupafya@users.noreply.github.com>
2023-04-16 00:18:11 +0300

    Add files via upload

--- simplenote_backup.py
+++ simplenote_backup.py
@@ -2,13 +2,19 @@ import simplenote
 import os
 import re
 import datetime
+import base64
 
 
+# 1. string encoded mail    2. string encoded password
+vars =["encoded mail here", "encoded password here="]
+
+# Path to desktop directory
+main_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+# Root folder name
 root_folder_name = "Simplenote Backup"
-mail = "e-mail"
-passw = "password"
 
-
+# Name of each backup
 sub_folder_name = "simplenote_backup_"+datetime.date.today().strftime('%d-%m-%Y')
 
 
@@ -16,28 +22,41 @@ sub_folder_name = "simplenote_backup_"+datetime.date.today().strftime('%d-%m-%Y'
 
 
 
-sn = simplenote.Simplenote(mail, passw)
+
+
+
+
+
+
+
+
+
+
+sn = simplenote.Simplenote(base64.b64decode(vars[0].encode()).decode('utf-8'), base64.b64decode(vars[1].encode()).decode('utf-8'))
 note_list = sn.get_note_list()
 
-def clean_file_name(name):
-    # Remove invalid characters
-    cleaned_name = re.sub('[\\/:\*\?"<>\|]', '', name)
-    # Remove leading and trailing whitespace
-    cleaned_name = cleaned_name.strip()
-    # Shorten the string if it exceeds 260 characters
-    if len(cleaned_name) > 260:
-        cleaned_name = cleaned_name[:260]
-    return cleaned_name
-
-
-desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') # Gets path to desktop directory
-root_folder_path = os.path.join(desktop_path, root_folder_name) # Creates new path to folder on desktop
-if not os.path.exists(root_folder_path):
-    os.makedirs(root_folder_path)
-desktop_path=root_folder_path
 
 if (sn):
-    new_folder_path = os.path.join(desktop_path, sub_folder_name)  # Creates new path to folder on desktop
+
+    def clean_file_name(name):
+        # Remove invalid characters
+        cleaned_name = re.sub('[\\/:\*\?"<>\|]', '', name)
+        # Remove leading and trailing whitespace
+        cleaned_name = cleaned_name.strip()
+        # Shorten the string if it exceeds 260 characters
+        if len(cleaned_name) > 260:
+            cleaned_name = cleaned_name[:260]
+        return cleaned_name
+
+
+
+
+    root_folder_path = os.path.join(main_path, root_folder_name)  # Creates new path to folder on desktop
+    if not os.path.exists(root_folder_path):
+        os.makedirs(root_folder_path)
+    main_path = root_folder_path
+    txt_files=[]
+    new_folder_path = os.path.join(main_path, sub_folder_name)  # Creates new path to folder on desktop
     if not os.path.exists(new_folder_path):
         os.makedirs(new_folder_path)
 
@@ -45,12 +64,12 @@ if (sn):
         if (note.get("deleted") == 0):
             content = note.get("content").rstrip('\r\n') + "\n\n"
             title = clean_file_name(content.split('\n')[0])
-            file = open(os.path.join(new_folder_path, title) + ".txt", "w", encoding="utf-8")
+            filename = os.path.join(new_folder_path, title) + ".txt"
+            file = open(filename, "w", encoding="utf-8")
             file.write(content)
             file.close()
+            txt_files.append(title+".txt")
 
-
-
-
-
-
+    for i in os.listdir(new_folder_path):
+        if i not in txt_files:
+            os.remove(os.path.join(new_folder_path,i))
